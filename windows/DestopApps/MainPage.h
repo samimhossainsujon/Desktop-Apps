05b2acdcc7450c31a53bb715c8010cb4565b9694
samimhossainsujon <samimhossainsanto@gmail.com>
2024-02-16 01:36:35 +0600

    Initial commit
    Generated by react-native@0.73.4

---
+++ windows/DestopApps/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::DestopApps::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::DestopApps::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
