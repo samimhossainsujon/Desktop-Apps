c583f3bf247bb2d667564894b081ad8f72ae1dc5
samimhossainsujon <samimhossainsanto@gmail.com>
2024-03-02 23:09:35 +0600

    v-update

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 const App = () => {
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>App</Text>
+      <Text>final test </Text>
     </View>
   );
 };
